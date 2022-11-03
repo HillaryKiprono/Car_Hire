502471653c16a56f0474b2b1c5a8817f60f0de67
Hillary Kiprono <kipronohillary240@gmail.com>
2022-11-03 11:16:37 +0300

    save car details to firebase

--- app/src/main/java/com/kipronohillary/carhire/activities/InsertNewCarActivity.kt
+++ app/src/main/java/com/kipronohillary/carhire/activities/InsertNewCarActivity.kt
@@ -8,9 +8,15 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.text.TextUtils
 import android.util.Log
-import com.kipronohillary.carhire.R
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.kipronohillary.carhire.activities.models.CarDetails
 import com.kipronohillary.carhire.databinding.ActivityInsertNewCarBinding
+import java.util.UUID
 
 class InsertNewCarActivity : AppCompatActivity() {
     private lateinit var binding:ActivityInsertNewCarBinding
@@ -42,7 +48,57 @@ class InsertNewCarActivity : AppCompatActivity() {
     }
 
     private fun performDataValidation() {
-        TODO("Not yet implemented")
+        c_name=binding.carName.text.toString()
+        c_model=binding.carModel.text.toString()
+        c_hire_price=binding.carPrice.text.toString()
+        c_seat_capacity=binding.carSeatingCapacity.text.toString()
+
+
+        if(TextUtils.isEmpty(c_name)){
+            Toast.makeText(this, "Car name required", Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(c_model)){
+            Toast.makeText(this, "car Model required", Toast.LENGTH_SHORT).show()
+        }
+
+        else if(TextUtils.isEmpty(c_hire_price)){
+            Toast.makeText(this, "car Hire Price required", Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(c_seat_capacity)){
+            Toast.makeText(this, "car Seating Capacity required", Toast.LENGTH_SHORT).show()
+        }
+        else{
+
+            uploadImageToFirebaseStorage()
+
+        }
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+        progressDialog.show()
+        if(selectedPhotoUri==null)return
+        val filename=UUID.randomUUID().toString()
+        val ref=FirebaseStorage.getInstance().getReference("/CarImages/$filename")
+        ref.putFile(selectedPhotoUri!!)
+            .addOnCompleteListener{
+                ref.downloadUrl.addOnSuccessListener {
+                    saveCarDetailsToRealtime(it.toString())
+                }
+            }
+    }
+
+    private fun saveCarDetailsToRealtime(carImageUrl: String) {
+       progressDialog.show()
+        val uid=FirebaseAuth.getInstance().uid?:""
+        var databaseReference=FirebaseDatabase.getInstance().getReference("/Car_Items/$uid").push()
+        var carItem= CarDetails(uid,c_name,c_model,c_hire_price,c_seat_capacity,carImageUrl)
+        databaseReference.setValue(carItem).addOnSuccessListener { 
+            progressDialog.dismiss()
+            Toast.makeText(this, "New Car item Saved Successfully", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener { 
+            progressDialog.dismiss()
+            Toast.makeText(this, "Failed to Save Car Items to firebase", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun launchGallery() {
---
+++ app/src/main/java/com/kipronohillary/carhire/activities/models/CarDetails.kt
@@ -0,0 +1,11 @@
+package com.kipronohillary.carhire.activities.models
+
+class CarDetails(
+    val uid: String,
+    val car_name: String,
+    val car_model: String,
+    val car_hire_price: String,
+    val car_seat_capacity: String,
+    val carImageUrl: String
+) {
+}
