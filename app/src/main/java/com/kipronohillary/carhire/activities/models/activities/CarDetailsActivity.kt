4e002e614084b4f5629f4e726d3ee5e941fc7eca
Hillary Kiprono <kipronohillary240@gmail.com>
2022-11-03 18:26:55 +0300

    view Car

---
+++ app/src/main/java/com/kipronohillary/carhire/activities/models/activities/CarDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.kipronohillary.carhire.activities.models.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kipronohillary.carhire.R
+
+class CarDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_details)
+    }
+}
